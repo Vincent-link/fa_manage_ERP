093b9efd6e0bf229b8b9b774cf503f0fe0b34d29
闫涛 <taoyan@huaxing.com>
2020-06-23 17:03:00 +0800

    fix

--- app/api/entities/attachment.rb
+++ app/api/entities/attachment.rb
@@ -3,10 +3,10 @@ module Entities
     expose :id, documentation: {type: 'integer', desc: '文件id'}
     expose :filename, documentation: {type: 'string', desc: '文件名'}
     expose :organization, if: lambda { |ins, options| options[:organizations].present?}, documentation: {type: 'Entities::IdName', desc: '机构'} do |ins, options|
-      organization = options[:organizations][ins.record_id].first
+      organization = options[:organizations][ins.record_id]
       {
           id: organization&.id,
-          name: organization&:name
+          name: organization&.name
       }
     end
     expose :blob, using: Entities::BlobFile
