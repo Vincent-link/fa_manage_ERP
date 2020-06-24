219c26a902fb9ea56e114c32c427501db196e084
JarvisLi <jarvisli@huaxing.com>
2020-06-24 11:12:07 +0800

    fix

--- app/api/entities/ecm_group_for_show.rb
+++ app/api/entities/ecm_group_for_show.rb
@@ -2,6 +2,7 @@ module Entities
   class EcmGroupForShow < Base
     expose :id, documentation: {type: 'integer', desc: 'group_id', required: true}
     expose :name, documentation: {type: 'string', desc: 'group名称', required: true}
+    expose :sector_ids, documentation: {type: 'integer', desc: '关注行业', is_array: true}
     expose :investor_group_organizations, using: Entities::EcmGroupOrganization
   end
 end
