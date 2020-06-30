d032ce34615b81ec7dae64d565cd90c69ca168bb
JarvisLi <jarvisli@huaxing.com>
2020-06-30 18:49:29 +0800

    go

--- config/initializers/zombie.rb
+++ config/initializers/zombie.rb
@@ -1,3 +1,3 @@
-# Zombie.host = "http://router.canary.huaxing.com:9787"
-Zombie.host = "http://localhost:8780"
+Zombie.host = "http://router.canary.huaxing.com:9787"
+#Zombie.host = "http://localhost:8780"
 Zombie.app_name = 'magazine_dev'
