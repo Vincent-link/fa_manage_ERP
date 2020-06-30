a195ebd9f2501588b82a75630fcf8fe205f0e0d6
闫涛 <taoyan@huaxing.com>
2020-06-30 16:58:24 +0800

    fix

--- app/api/entities/company_base_info.rb
+++ app/api/entities/company_base_info.rb
@@ -2,7 +2,7 @@ module Entities
   class CompanyBaseInfo < Base
     expose :id, documentation: {type: 'integer', desc: '公司id'}
     expose :name, documentation: {type: 'string', desc: '公司名称'}
-    expose :logo, using: Entities::File,if: ->(ins) {ins.logo.present?}, documentation: {type: Entities::File, desc: 'logo'}
+    expose :logo_attachment, as: :logo, using: Entities::File, documentation: {type: Entities::File, desc: '公司头像', required: true}
     expose :one_sentence_intro, documentation: {type: 'string', desc: '一句话简介'}
     expose :location_province_id, documentation: {type: 'integer', desc: '省份'}
     expose :location_city_id, documentation: {type: 'integer', desc: '城市'}
