ff31d5a324fca64be2efb4ad89b80c42a0094887
JarvisLi <jarvisli@huaxing.com>
2020-06-30 11:01:23 +0800

    go

--- app/api/entities/address.rb
+++ app/api/entities/address.rb
@@ -3,6 +3,9 @@ module Entities
     expose :id, documentation: {type: 'integer', desc: '地址id'}
     expose :owner_id, documentation: {type: 'integer', desc: '创建人'}
     expose :location_id, documentation: {type: 'integer', desc: '地域id'}
+    expose :province_id, documentation: {type: 'integer', desc: '地域id'} do |ins|
+      CacheBox.dm_locations[ins.location_id]&.parent_id
+    end
     expose :address_desc, documentation: {type: 'string', desc: '地址id'}
 
     with_options(format_with: :dm_time_to_s_second) do
