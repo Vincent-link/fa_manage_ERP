ddb8742f6c2478fb250796b8358683387ac66775
JarvisLi <jarvisli@huaxing.com>
2020-07-01 11:17:54 +0800

    go

--- app/api/entities/base.rb
+++ app/api/entities/base.rb
@@ -1,8 +1,17 @@
 module Entities
   class Base < Grape::Entity
-    format_with(:time_to_s_date) {|dt| dt&.to_s(:date)}
-    format_with(:time_to_s_second) {|dt| dt&.to_s(:second)}
-    format_with(:time_to_s_minute) {|dt| dt&.to_s(:minute)}
+    format_with(:time_to_s_date) do |dt|
+      dt = Time.parse(dt) if dt.is_a? String
+      dt&.to_s(:date)
+    end
+    format_with(:time_to_s_second) do |dt|
+      dt = Time.parse(dt) if dt.is_a? String
+      dt&.to_s(:second)
+    end
+    format_with(:time_to_s_minute) do |dt|
+      dt = Time.parse(dt) if dt.is_a? String
+      dt&.to_s(:minute)
+    end
     format_with(:dm_time_to_s_second) {|dt| Time.parse(dt)&.to_s(:second)}
   end
 end
