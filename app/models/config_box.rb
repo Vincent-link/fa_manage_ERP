7d5e48787e71cd53f2d254cdd935900d0d446881
JarvisLi <jarvisli@huaxing.com>
2020-07-03 13:20:59 +0800

    go

--- app/models/config_box.rb
+++ app/models/config_box.rb
@@ -5,6 +5,8 @@ class ConfigBox
       organization_logo:        { value: 'organization_logo', desc: "机构logo"  , is_static: true},
       member_logo:              { value: 'member_logo',       desc: "投资人logo", is_static: true},
       company_logo:             { value: 'company_logo',      desc: "公司logo",   is_static: true},
-      knowledge_base_file:      { value: 'knowledge_base_file',      desc: "知识库文件",   is_static: false}
+      knowledge_base_file:      { value: 'knowledge_base_file',      desc: "知识库文件",   is_static: false},
+      member_avatar:            { value: 'member_avatar',       desc: "投资人头像", is_static: true},
+      member_card:            { value: 'member_card',       desc: "投资人名片", is_static: false},
   }
 end
