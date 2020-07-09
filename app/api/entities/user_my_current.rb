78d209fbfdf177426011336ad39cd215d315f2a4
JarvisLi <jarvisli@huaxing.com>
2020-07-09 11:35:24 +0800

    go

--- app/api/entities/user_my_current.rb
+++ app/api/entities/user_my_current.rb
@@ -1,14 +1,11 @@
 module Entities
   class UserMyCurrent < Base
     expose :id, documentation: {type: 'integer', desc: '用户id', required: true}
-    expose :name, documentation: {type: 'string', desc: '用户姓名', required: true} do |user, options|
-      if options[:type].nil?
-        user.name
-      else
-        options[:type] == "yes" ? user.name : ""
-      end
-    end
+    expose :name, documentation: {type: 'string', desc: '用户姓名', required: true}
     expose :avatar_attachment, as: :avatar, using: Entities::File, documentation: {type: Entities::File, desc: '用户头像', required: true}
+    expose :has_sub_user, documentation: {type: 'boolean', desc: '用户id', required: true} do |user|
+      user.sub_users.exists?
+    end
     expose :funding_status_sort, documentation: {type: 'integer', desc: '状态排序', is_array: true} do |ins|
       ins.funding_status_sort || 'Funding'.constantize.status_values
     end
---
+++ db/migrate/20200709032619_pipeline_v2.rb
@@ -0,0 +1,4 @@
+class PipelineV2 < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
