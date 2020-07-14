39a6c326a99cefd46e8804072c3a9e8b8e3a1ab7
34 <34>
2020-07-14 11:20:13 +0800

    添加时间

--- app/api/entities/answer.rb
+++ app/api/entities/answer.rb
@@ -4,5 +4,6 @@ module Entities
     expose :desc, documentation: {type: 'string', desc: '描述', required: true}
     expose :user, using: Entities::User
     expose :question_id, documentation: {type: 'integer', desc: '问题id', required: true}
+    expose :created_at, documentation: {type: 'integer', desc: '创建时间', required: true}
   end
 end
