feb2bc107806af666b73ce9bd61179193f3ba4e0
闫涛 <taoyan@huaxing.com>
2020-07-15 10:19:29 +0800

    fix

--- app/api/entities/track_log_detail.rb
+++ app/api/entities/track_log_detail.rb
@@ -9,8 +9,8 @@ module Entities
     end
     expose :linkable_id, documentation: {type: 'integer', desc: '约见id'}
     expose :linkable_type, documentation: {type: 'string', desc: '类型目前只有约见'}
-    expose :history, with: Entities::TrackLogDetailHistorySpa, as: :spa_history, if: lambda { |ins| ins.detail_type_spa?}, documentation: {type: Entities::TrackLogDetailHistorySpa, desc: 'SPA类型的历史记录'}
-    expose :history, with: Entities::TrackLogDetailHistorySpa, as: :ts_history, if: lambda { |ins| ins.detail_type_ts?}, documentation: {type: Entities::TrackLogDetailHistoryTs, desc: 'TS类型的历史记录'}
-    expose :history, with: Entities::TrackLogDetailHistorySpa, as: :calendar_history, if: lambda { |ins| ins.detail_type_calendar?}, documentation: {type: Entities::TrackLogDetailHistoryCalendar, desc: '约见类型的历史记录'}
+    expose :history, as: :spa_history, if: lambda { |ins| ins.detail_type_spa?}, documentation: {type: Entities::TrackLogDetailHistorySpa, desc: 'SPA类型的历史记录'}
+    expose :history, as: :ts_history, if: lambda { |ins| ins.detail_type_ts?}, documentation: {type: Entities::TrackLogDetailHistoryTs, desc: 'TS类型的历史记录'}
+    expose :history, as: :calendar_history, if: lambda { |ins| ins.detail_type_calendar?}, documentation: {type: Entities::TrackLogDetailHistoryCalendar, desc: '约见类型的历史记录'}
   end
 end
