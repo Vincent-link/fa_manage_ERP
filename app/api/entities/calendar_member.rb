217b89be31bdd6d6ab3d38cddd1ee5e93542b283
JarvisLi <jarvisli@huaxing.com>
2020-07-17 18:32:46 +0800

    go

--- app/api/entities/calendar_member.rb
+++ app/api/entities/calendar_member.rb
@@ -5,5 +5,10 @@ module Entities
     expose :memberable_name
     expose :memberable_type
     expose :memberable_position
+    expose :ir_review do |ins|
+      if ins.calendar.meeting_category_org_meeting?
+        ins.memberable.ir_review if ins.memberable.is_a? Member
+      end
+    end
   end
 end
