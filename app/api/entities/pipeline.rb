842d92918a3ecc45fec60ae24397da8de0ffdb5a
JarvisLi <jarvisli@huaxing.com>
2020-07-17 13:47:10 +0800

    go

--- app/api/entities/pipeline.rb
+++ app/api/entities/pipeline.rb
@@ -5,20 +5,19 @@ module Entities
     expose :funding_id, documentation: {type: 'integer', desc: '项目id'}
     expose :funding_status_desc, documentation: {type: 'string', desc: '项目状态'}
     expose :status, documentation: {type: 'integer', desc: '所处阶段'}
-    expose :est_amount, documentation: {type: 'integer', desc: '预期融资金额'}
+    expose :est_amount, documentation: {type: 'float', desc: '预期融资金额'}
     expose :est_amount_currency, documentation: {type: 'integer', desc: '预期融资金额币种'}
-    expose :fee_rate, documentation: {type: 'integer', desc: '费率'}
-    expose :fee_discount, documentation: {type: 'integer', desc: '费率折扣'}
-    expose :other_amount, documentation: {type: 'integer', desc: '其他金额'}
-    expose :other_amount_currency, documentation: {type: 'integer', desc: '其他金额币种'}
-    expose :complete_rate, documentation: {type: 'integer', desc: '年内完成概率'}
-    expose :total_fee, documentation: {type: 'integer', desc: '项目总收入预测'}
-    expose :total_fee_currency, documentation: {type: 'integer', desc: '项目总收入币种'}
-    expose :currency_rate, documentation: {type: 'integer', desc: '汇率'}
+    expose :fee_rate, documentation: {type: 'float', desc: '费率'}
+    expose :other_amount, documentation: {type: 'float', desc: '其他金额'}
+    expose :complete_rate, documentation: {type: 'float', desc: '年内完成概率'}
+    expose :total_fee, documentation: {type: 'float', desc: '项目总收入预测'}
+    expose :total_fee_currency, documentation: {type: 'integer', desc: '收入币种'}
+    expose :currency_rate, documentation: {type: 'float', desc: '汇率'}
     expose :el_date, documentation: {type: 'date', desc: '签约日期'}
     expose :est_bill_date, documentation: {type: 'date', desc: '预计账单日期'}
     expose :user_name, documentation: {type: 'string', desc: '更新人'}
     expose :name, documentation: {type: 'string', desc: '名称'}
+    expose :success_fee, documentation: {type: 'float', desc: '成功费'}
 
     expose :pipeline_divides, using: Entities::PipelineDivide, documentation: {type: Entities::PipelineDivide, desc: '分成'}
     expose :payments, using: Entities::Payment, documentation: {type: Entities::Payment, desc: '账单'}
