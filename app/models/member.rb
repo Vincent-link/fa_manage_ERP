cf60b7db66b08de65c5f96154ceed462ad81583d
34 <34>
2020-07-22 19:36:39 +0800

    适用范围

--- app/models/member.rb
+++ app/models/member.rb
@@ -233,7 +233,6 @@ class Member < ApplicationRecord
       self.member_user_relations.map {|e| Notification.create(notification_type: Notification.notification_type_value("investor"), content: content, user_id: e.user_id, is_read: false, notice: {member_id: self.id}) if content.present?}
     end
 
-
     if self.previous_changes[:organization_id].present? && self.previous_changes[:organization_id][0].present?
       before = Organization.find(self.previous_changes[:organization_id][0])
       after = Organization.find(self.previous_changes[:organization_id][1])
