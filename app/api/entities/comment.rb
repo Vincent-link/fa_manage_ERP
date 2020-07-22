bc95eaa9329ac4584d65d293b5a691a737e50c6f
JarvisLi <jarvisli@huaxing.com>
2020-07-22 18:28:11 +0800

    go

--- app/api/entities/comment.rb
+++ app/api/entities/comment.rb
@@ -7,7 +7,9 @@ module Entities
 
     expose :user, using: Entities::User
     expose :relate_user_ids do |ins|
-      CacheBox.user_cache.slice(*ins.relate_user_ids)
+      CacheBox.user_cache.slice(*ins.relate_user_ids).map do |k, v|
+        {id: k, name: v}
+      end
     end
 
     with_options(format_with: :time_to_s_minute) do
