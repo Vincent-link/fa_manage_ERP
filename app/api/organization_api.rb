41b302b642818aebab4db20b7c50ed05d44ff974
JarvisLi <jarvisli@huaxing.com>
2020-07-23 19:26:35 +0800

    go

--- app/api/organization_api.rb
+++ app/api/organization_api.rb
@@ -225,9 +225,16 @@ class OrganizationApi < Grape::API
       params do
         requires :page, type: Integer, desc: '页数', default: 1
         requires :page_size, as: :per_page, type: Integer, desc: '每页条数', default: 10
+        optional :sector_ids, type: Array[Integer], desc: '行业'
+        optional :round_ids, type: Array[Integer], desc: '轮次'
+        optional :status, type: Array[Integer], desc: '状态'
       end
       get :untrack_funding do
-        present Funding.left_joins(:track_logs).where("track_logs.organization_id != #{@organization.id} or track_logs.organization_id is null").where(status: [Funding.status_pursue_value, Funding.status_execution_value, Funding.status_closing_value]), with: Entities::FundingForUntrack, members: @organization.members
+        fundings = Funding.left_joins(:track_logs).where("track_logs.organization_id != #{@organization.id} or track_logs.organization_id is null").where(status: [Funding.status_pursue_value, Funding.status_execution_value, Funding.status_closing_value])
+        fundings = fundings.where(status: params[:status]) if params[:status].present?
+        fundings = fundings.where(round_id: params[:round_ids]) if params[:round_ids].present?
+        fundings = fundings.includes(:company).where(companies: {sector_id: params[:sector_ids]}) if params[:sector_ids].present?
+        present fundings.paginate(page: params[:page], per_page: params[:per_page]), with: Entities::FundingForUntrack, members: @organization.members
       end
 
       desc 'portfollo'
@@ -237,7 +244,7 @@ class OrganizationApi < Grape::API
       end
       get :portfollo do
         funding_ids, company_ids = Funding.includes(:track_logs).where(track_logs: {status: TrackLog.status_spa_sha, organization_id: @organization.id}).pluck(:id, :company_id).transpose
-        present Funding.where(company_id: company_ids, status: [Funding.status_pursue_value, Funding.status_execution_value, Funding.status_closing_value]).where.not(id: funding_ids), with: Entities::FundingForUntrack, members: @organization.members
+        present Funding.where(company_id: company_ids, status: [Funding.status_pursue_value, Funding.status_execution_value, Funding.status_closing_value]).where.not(id: funding_ids).paginate(page: params[:page], per_page: params[:per_page]), with: Entities::FundingForUntrack, members: @organization.members
       end
     end
   end
