555324f6264d5989b7c6295a6edf6dc3357e48f6
JarvisLi <jarvisli@huaxing.com>
2020-07-23 11:36:13 +0800

    go

--- app/api/entities/user_for_index.rb
+++ app/api/entities/user_for_index.rb
@@ -5,7 +5,9 @@ module Entities
     expose :email, documentation: {type: 'string', desc: '用户邮箱', required: true}
     expose :team_name, documentation: {type: 'integer', desc: '团队', required: true}
     expose :bu_id, documentation: {type: 'integer', desc: '部门', required: true}
-    expose :leader, using: Entities::UserLite
+    expose :leader, documentation: {type: Entities::UserLite, desc: 'leader', required: true} do |ins|
+      {id: ins.leader_id, name: CacheBox.user_cache[ins.leader_id]} if ins.leader_id
+    end
     expose :user_title, using: Entities::UserLite
     expose :grade_name, documentation: {type: 'integer', desc: '内部职务', required: true}
     expose :roles, using: Entities::Role
