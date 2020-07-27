539b1601798771893eb00539671a719c7616eec7
JarvisLi <jarvisli@huaxing.com>
2020-07-27 19:53:40 +0800

    whenever

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,9 +5,9 @@ set :application, "magazine"
 set :repo_url, "git@10.101.7.1:imt/magazine.git"
 
 set :use_sudo, true
-set :linked_files, %w{config/database.yml config/master.key config/schedule.rb config/honeybadger.yml} << "config/settings/#{fetch(:stage)}.yml"
+set :linked_files, %w{config/database.yml config/master.key config/honeybadger.yml} << "config/settings/#{fetch(:stage)}.yml"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'public', 'tmp/cache', 'tmp/pids')
-set :whenever_identifier, -> {"#{fetch(:application)}_#{fetch(:stage)}#{fetch(:test_env_no)}"}
+set :whenever_identifier, -> {"#{fetch(:application)}_#{fetch(:stage)}"}
 
 ## sidekiq
 set :sidekiq_config, -> {"#{current_path}/config/sidekiq.yml"}
