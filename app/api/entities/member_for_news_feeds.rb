e512ff066eb8634d4cc6a141262209c6460c062c
Theo Chen <tychen@huaxing.com>
2020-07-28 13:30:09 +0800

    entity option

--- app/api/entities/member_for_news_feeds.rb
+++ app/api/entities/member_for_news_feeds.rb
@@ -10,25 +10,33 @@ module Entities
     expose :organization_logo, documentation: {type: 'string', desc: '机构logo url'} do |ins|
       ins.organization&.logo_attachment&.service_url
     end
-    expose :previous_organization_id, if: {type: PaperTrail::Version.news_feeds_type_value("member_change_org")}, documentation: {type: 'integer', desc: '更改前机构id'} do |_ins, opt|
-      opt[:changes]["organization_id"].first
+
+    with_options(if: {type: PaperTrail::Version.news_feeds_type_value("member_change_org")}) do
+      expose :previous_organization_id, documentation: {type: 'integer', desc: '更改前机构id'} do |_ins, opt|
+        opt[:changes]["organization_id"].first
+      end
+      expose :previous_organization_name, documentation: {type: 'string', desc: '更改前机构名称'} do |_ins, opt|
+        Organization.find_by_id(opt[:changes]["organization_id"].first)&.name
+      end
+      expose :following_organization_id, documentation: {type: 'integer', desc: '更改后机构id'} do |_ins, opt|
+        opt[:changes]["organization_id"].last
+      end
+      expose :following_organization_name, documentation: {type: 'string', desc: '更改后机构名称'} do |_ins, opt|
+        Organization.find_by_id(opt[:changes]["organization_id"].last)&.name
+      end
     end
-    expose :previous_organization_name, if: {type: PaperTrail::Version.news_feeds_type_value("member_change_org")}, documentation: {type: 'string', desc: '更改前机构名称'} do |_ins, opt|
-      Organization.find_by_id(opt[:changes]["organization_id"].first)&.name
-    end
-    expose :following_organization_id, if: {type: PaperTrail::Version.news_feeds_type_value("member_change_org")}, documentation: {type: 'integer', desc: '更改后机构id'} do |_ins, opt|
-      opt[:changes]["organization_id"].last
-    end
-    expose :following_organization_name, if: {type: PaperTrail::Version.news_feeds_type_value("member_change_org")}, documentation: {type: 'string', desc: '更改后机构名称'} do |_ins, opt|
-      Organization.find_by_id(opt[:changes]["organization_id"].last)&.name
-    end
+
     expose :position_rank_id, as: :position, documentation: {type: 'integer', desc: '职级'}
-    expose :previous_position, if: {type: PaperTrail::Version.news_feeds_type_value("member_change_position")}, documentation: {type: 'integer', desc: '更改前职级'} do |_ins, opt|
-      opt[:changes]["position_rank_id"].first
+
+    with_options(if: {type: PaperTrail::Version.news_feeds_type_value("member_change_position")}) do
+      expose :previous_position, documentation: {type: 'integer', desc: '更改前职级'} do |_ins, opt|
+        opt[:changes]["position_rank_id"].first
+      end
+      expose :following_position, documentation: {type: 'integer', desc: '更改后职级'} do |_ins, opt|
+        opt[:changes]["position_rank_id"].last
+      end
     end
-    expose :following_position, if: {type: PaperTrail::Version.news_feeds_type_value("member_change_position")}, documentation: {type: 'integer', desc: '更改后职级'} do |_ins, opt|
-      opt[:changes]["position_rank_id"].last
-    end
+
     expose :round_ids, documentation: {type: 'integer', desc: '关注轮次', is_array: true}
     expose :sector_ids, documentation: {type: 'integer', desc: '关注行业', is_array: true}
   end
--- app/api/entities/organization_for_news_feeds.rb
+++ app/api/entities/organization_for_news_feeds.rb
@@ -1,22 +1,26 @@
 module Entities
   class OrganizationForNewsFeeds < Base
-    expose :id, if: {type: PaperTrail::Version.news_feeds_type_value("org_create")}, documentation: {type: 'integer', desc: '机构id'}
-    expose :name, if: {type: PaperTrail::Version.news_feeds_type_value("org_create")}, documentation: {type: 'string', desc: '机构名称'}
-    expose :logo, if: {type: PaperTrail::Version.news_feeds_type_value("org_create")}, documentation: {type: 'string', desc: '机构logo url'} do |ins|
-      ins.logo_attachment&.service_url
+    with_options(if: {type: PaperTrail::Version.news_feeds_type_value("org_create")}) do
+      expose :id, documentation: {type: 'integer', desc: '机构id'}
+      expose :name, documentation: {type: 'string', desc: '机构名称'}
+      expose :logo, documentation: {type: 'string', desc: '机构logo url'} do |ins|
+        ins.logo_attachment&.service_url
+      end
+      expose :round_ids, documentation: {type: 'integer', desc: '关注轮次', is_array: true}
+      expose :sector_ids, documentation: {type: 'integer', desc: '关注行业', is_array: true}
     end
-    expose :round_ids, if: {type: PaperTrail::Version.news_feeds_type_value("org_create")}, documentation: {type: 'integer', desc: '关注轮次', is_array: true}
-    expose :sector_ids, if: {type: PaperTrail::Version.news_feeds_type_value("org_create")}, documentation: {type: 'integer', desc: '关注行业', is_array: true}
 
-    expose :id, if: {type: PaperTrail::Version.news_feeds_type_value("org_ir_review")}, documentation: {type: 'integer', desc: '机构id'} do |ins|
-      ins.commentable.id
+    with_options(if: {type: PaperTrail::Version.news_feeds_type_value("org_ir_review")}) do
+      expose :id, documentation: {type: 'integer', desc: '机构id'} do |ins|
+        ins.commentable.id
+      end
+      expose :name, documentation: {type: 'string', desc: '机构名称'} do |ins|
+        ins.commentable.name
+      end
+      expose :logo, documentation: {type: 'string', desc: '机构logo url'} do |ins|
+        ins.commentable&.logo_attachment&.service_url
+      end
+      expose :content, documentation: {type: 'string', desc: 'IrReview'}
     end
-    expose :name, if: {type: PaperTrail::Version.news_feeds_type_value("org_ir_review")}, documentation: {type: 'string', desc: '机构名称'} do |ins|
-      ins.commentable.name
-    end
-    expose :logo, as: :logo, if: {type: PaperTrail::Version.news_feeds_type_value("org_ir_review")}, documentation: {type: 'string', desc: '机构logo url'} do |ins|
-      ins.commentable&.logo_attachment&.service_url
-    end
-    expose :content, if: {type: PaperTrail::Version.news_feeds_type_value("org_ir_review")}, documentation: {type: 'string', desc: 'IrReview'}
   end
 end
